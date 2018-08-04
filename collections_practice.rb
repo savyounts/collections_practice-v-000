1ab4ff3c72735c90ec1eed3cb9c88780318ef2ba
Sav Younts <savyounts@gmail.com>
2018-08-04 16:03:48 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,4 +19,8 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end 
+
+def swap_elements_from_to (array, index, destination_index)
+  array[index] = array[desination_index]
+end 
   
