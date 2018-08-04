1db7c5289cdc88cee3f21c29e3417ff7cf8b309b
Sav Younts <savyounts@gmail.com>
2018-08-04 16:33:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,6 +51,9 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    element << "s" unless index == 1 
+    if index != 1 
+      element << "s" 
+    else 
+      array[1]
   end
 end 
